f40e38ce519024a8007c1774d5bb4aa106ad7796
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-03-11 16:28:43 +0000

    use regex

--- JavaScript/form control/srcript.js
+++
@@ -1,8 +0,0 @@
-const inputPassword = document.querySelector("#inputPassword4");
-
-const password = () => {
-  if (inputPassword.value.length < 8) {
-    alert("La contraseña debe contener al menos 8 caracteres");
-  }
-  alert(`Contraseña: ${inputPassword.value}`);
-};
